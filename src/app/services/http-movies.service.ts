8fd8f40bfbe939f7d735267b91daa2efec953eed
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2023-11-14 14:31:55 +0100

    add GET method

--- src/app/services/http-movies.service.ts
+++ src/app/services/http-movies.service.ts
@@ -1,10 +1,27 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Movie} from '../models/movie';
+import {tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpMoviesService {
 
+  private url = 'http://localhost:3000';
+
   constructor(private http: HttpClient) { }
+
+  getMovies(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(this.url + '/movies').pipe(tap(console.log));
+  }
+
+  // Dodatkowa konfiguracja w zapytaniu
+  // getMovies(): Observable<HttpResponse<Movie[]>> {
+  //   return this.http
+  //     .get<HttpResponse<Movie[]>>(this.url + '/movies', {observe: 'response'})
+  //     .pipe(tap(console.log));
+  // }
+
 }
