5b0a07e39ece564ccb3dcbd3529e86b3c500e15f
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2023-11-15 12:58:37 +0100

    add handle errors and show error message

--- src/app/services/http-movies.service.ts
+++ src/app/services/http-movies.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpResponse} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {HttpClient, HttpErrorResponse, HttpResponse} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
 import {Movie} from '../models/movie';
-import {tap} from 'rxjs/operators';
+import {catchError, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -42,6 +42,21 @@ export class HttpMoviesService {
   deleteMovie(id: string): Observable<{}> {
     return this.http.delete<{}>(this.url + '/' + id)
       .pipe(tap(console.log));
+  }
+
+  makeError(): Observable<HttpErrorResponse> {
+    return this.http
+      .delete(this.url + '/' + 999)
+      .pipe(tap(console.log), catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    console.error(
+      `Name: ${error.name} \n` +
+      `Message: ${error.message} \n` +
+      `Returned code: ${error.status} \n`
+    );
+    return throwError('Something bad happened; please try again later.');
   }
 
 }
