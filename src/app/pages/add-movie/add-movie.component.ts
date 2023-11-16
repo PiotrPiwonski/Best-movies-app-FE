cbc1a59b1ae291f463acb0b64643eccba81204ba
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2023-11-16 13:02:03 +0100

    add select fields in form

--- src/app/pages/add-movie/add-movie.component.ts
+++ src/app/pages/add-movie/add-movie.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Movie} from '../../models/movie';
 import {NgForm} from '@angular/forms';
+import {HttpService} from '../../services/http.service';
 
 @Component({
   selector: 'app-add-movie',
@@ -8,12 +9,18 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./add-movie.component.css']
 })
 export class AddMovieComponent implements OnInit {
-
   model: Partial<Movie> = {};
+  categories: string[] = [];
+  years: string[] = [];
 
-  constructor() { }
+  constructor(public http: HttpService) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.http.getCategories()
+      .subscribe(categories => this.categories = categories);
+    this.http.getYears()
+      .subscribe(years => this.years = years);
+  }
 
   send() {
     console.log(this.model)
